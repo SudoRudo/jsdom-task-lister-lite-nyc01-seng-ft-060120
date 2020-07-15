b1ee2fa485ed7838b1a2dcf3b69315e031797958
shiro-han <shan99business@gmail.com>
2020-07-14 20:39:13 -0400

    REFACTORED

--- src/index.js
+++ src/index.js
@@ -1,57 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const ourForm = document.getElementById('create-task-form');
+  const tasks = document.getElementById('tasks');
+  const submitButton = document.getElementById('button');
+
+  let selector = document.createElement('select');
+  selector.setAttribute("id", "selector");
+
+  let red = document.createElement('option');
+  red.setAttribute("value", "red");
+  red.innerHTML = "High Priority";
+  selector.appendChild(red);
+
+  let yellow = document.createElement('option');
+  yellow.setAttribute("value", "yellow");
+  yellow.innerHTML = "Medium Priority";
+  selector.appendChild(yellow);
+
+  let green = document.createElement('option');
+  green.setAttribute("value", "green");
+  green.innerHTML = "Low Priority";
+  selector.appendChild(green);
+
+  submitButton.insertAdjacentElement('beforebegin', selector);
+
+  ourForm.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    let newTaskLi = document.createElement('li');
+
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML = "Delete";
+
+    deleteButton.addEventListener("click", function(e){
+      let taskLi = e.target.parentElement
+      taskLi.remove();
+    })
+    
+    newTaskLi.innerHTML = ourForm[0].value + ' ';
+    newTaskLi.appendChild(deleteButton);
+    newTaskLi.style.color = ourForm[1].value
+    tasks.appendChild(newTaskLi);
+  })
 });
 
-// let submitButton = document.getElementById('button');
-const ourForm = document.getElementById('create-task-form');
-const tasks = document.getElementById('tasks');
-const submitButton = document.getElementById('button');
 
-let selector = document.createElement('SELECT');
-selector.setAttribute("id", "selector");
 
-let red = document.createElement('option');
-red.setAttribute("value", "red");
-red.innerHTML = "High Priority";
-selector.appendChild(red);
-
-let yellow = document.createElement('option');
-yellow.setAttribute("value", "yellow");
-yellow.innerHTML = "Medium Priority";
-selector.appendChild(yellow);
-
-let green = document.createElement('option');
-green.setAttribute("value", "green");
-green.innerHTML = "Low Priority";
-selector.appendChild(green);
-
-submitButton.insertAdjacentElement('beforebegin', selector);
-
-let i = 0;
-
-ourForm.addEventListener("submit", function(e){
-  e.preventDefault();
-
-  let newTaskLi = document.createElement('li');
-
-  let deleteButton = document.createElement('BUTTON');
-  deleteButton.innerHTML = "Delete";
-  deleteButton.className = `${i}`;
-  i++;
+// // document.getElementsByTagName('BUTTON').addEventListener("click", function(e){
+//   let clickedDeleteButton = document.getElementsByClassName(e.target.className)[className]
+//   clickedDeleteButton.addEventListener("click", function(e){
+//     liTask = clickedDeleteButton.parentNode
+//     liTask.remove
   
-  newTaskLi.innerHTML = ourForm.elements[0].value + ' ';
-  newTaskLi.appendChild(deleteButton);
-  newTaskLi.style.color = ourForm.elements[1].value
-  tasks.appendChild(newTaskLi);
-})
-
-// document.getElementsByTagName('BUTTON').addEventListener("click", function(e){
-  let clickedDeleteButton = document.getElementsByClassName(e.target.className)[className]
-  clickedDeleteButton.addEventListener("click", function(e){
-    liTask = clickedDeleteButton.parentNode
-    liTask.remove
-  
-  })
+//   })
   
   
-// })
+// // })
